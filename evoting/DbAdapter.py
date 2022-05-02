c306a873855b152b171585f0c6da35ba01cef5fd
Bob Hsiao <bobhsiao.cs10g@nctu.edu.tw>
2022-05-02 16:14:30 +0800

    replace sqlite3 with pymysql

--- evoting/DbAdapter.py
+++ evoting/DbAdapter.py
@@ -1,64 +1,77 @@
-import sqlite3
+import pymysql
 import os
 from datetime import datetime
 
 TOKEN_SIZE = 4
 CHALLENGE_SIZE = 4
 
+db_settings = {
+    "host": "140.113.208.100",
+    "port": 3306,
+    "user": "root",
+    "password": "evoting2022",
+    "db": "evoting",
+    "charset": "utf8"
+}
+
 class DbAdapter:
     def __init__(self):
-        self.database = 'evoting.db'
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute("CREATE TABLE IF NOT EXISTS `Registration`(`name` text NOT NULL, \
+                                                                    `groups` text NOT NULL, \
+                                                                    `public_key` text NOT NULL)")
 
-        conn = sqlite3.connect(self.database)
-        conn.execute("CREATE TABLE IF NOT EXISTS Registration(name text NOT NULL, \
-                                                 groups text NOT NULL, \
-                                                 public_key text NOT NULL)")
+            cursor.execute("CREATE TABLE IF NOT EXISTS `Challenge`(`name` text NOT NULL, \
+                                                                   `challenge` text NOT NULL)")
 
-        conn.execute("CREATE TABLE IF NOT EXISTS Challenge(name text NOT NULL, \
-                                                           challenge text NOT NULL)")
+            cursor.execute("CREATE TABLE IF NOT EXISTS `Token`(`token` text NOT NULL, \
+                                                               `expired` text NOT NULL, \
+                                                               `name` text NOT NULL)")
 
-        conn.execute("CREATE TABLE IF NOT EXISTS Token(token text NOT NULL, \
-                                                       expired text NOT NULL, \
-                                                       name text NOT NULL)")
-
-        conn.execute("CREATE TABLE IF NOT EXISTS Election(election_name text NOT NULL, \
-                                                          end_date text NOT NULL, \
-                                                          groups text NOT NULL, \
-                                                          voters text NOT NULL)")
+            cursor.execute("CREATE TABLE IF NOT EXISTS `Election`(`election_name` text NOT NULL, \
+                                                                  `end_date` text NOT NULL, \
+                                                                  `groups` text NOT NULL, \
+                                                                  `voters` text NOT NULL)")
         conn.commit()
         conn.close()
 
     def add_register(self, name, group, public_key):
-        conn = sqlite3.connect(self.database)
-        data = conn.execute(f"SELECT groups,public_key from Registration WHERE name='{name}'").fetchone()
-        if data is not None:
-            status =  1  # Status.code=1 : Voter with the same name already exists
-        else:
-            key = int.from_bytes(public_key, byteorder='big', signed=False)
-            conn.execute(f"INSERT INTO Registration values('{name}','{group}','{key}')")
-            status =  0  # Status.code=0 : Successful registration
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT groups,public_key from Registration WHERE name='{name}'")
+            data = cursor.fetchone()
+            if data is not None:
+                status =  1  # Status.code=1 : Voter with the same name already exists
+            else:
+                key = int.from_bytes(public_key, byteorder='big', signed=False)
+                cursor.execute(f"INSERT INTO `Registration` values('{name}','{group}','{key}')")
+                status =  0  # Status.code=0 : Successful registration
         conn.commit()
         conn.close()
 
         return status
 
     def del_register(self, name):
-        conn = sqlite3.connect(self.database)
-        data = conn.execute(f"SELECT groups,public_key from Registration WHERE name='{name}'").fetchone()
-        if data is None:
-            status =  1  # Status.code=1 : No voter with the name exists on the server
-        else:
-            conn.execute(f"DELETE from Registration WHERE name='{name}'")
-            status =  0  # Status.code=0 : Successful unregistration
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT groups,public_key from Registration WHERE name='{name}'")
+            data = cursor.fetchone()
+            if data is None:
+                status =  1  # Status.code=1 : No voter with the name exists on the server
+            else:
+                cursor.execute(f"DELETE from Registration WHERE name='{name}'")
+                status =  0  # Status.code=0 : Successful unregistration
         conn.commit()
         conn.close()
 
         return status
 
     def get_register(self, name):
-        conn = sqlite3.connect(self.database)
-        data = conn.execute(f"SELECT groups,public_key from Registration WHERE name='{name}'").fetchone()
-        conn.commit()
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT groups,public_key from Registration WHERE name='{name}'")
+            data = cursor.fetchone()
         conn.close()
 
         if data is None:
@@ -69,17 +82,19 @@ class DbAdapter:
             return group, public_key
 
     def add_challenge(self, name, challenge):
-        conn = sqlite3.connect(self.database)
-        conn.execute(f"DELETE from Challenge WHERE name='{name}'")
-        challenge_toint = int.from_bytes(challenge, byteorder='big', signed=False)
-        conn.execute(f"INSERT INTO Challenge values('{name}','{challenge_toint}')")
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"DELETE from Challenge WHERE name='{name}'")
+            challenge_toint = int.from_bytes(challenge, byteorder='big', signed=False)
+            cursor.execute(f"INSERT INTO `Challenge` values('{name}','{challenge_toint}')")
         conn.commit()
         conn.close()
 
     def get_challenge(self, name):
-        conn = sqlite3.connect(self.database)
-        data = conn.execute(f"SELECT challenge from Challenge WHERE name='{name}'").fetchone()
-        conn.commit()
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT challenge from Challenge WHERE name='{name}'")
+            data = cursor.fetchone()
         conn.close()
 
         if data is None:
@@ -89,19 +104,21 @@ class DbAdapter:
             return challenge
 
     def add_token(self, token, expired, name):
-        conn = sqlite3.connect(self.database)
-        token_toint = int.from_bytes(token, byteorder='big', signed=False)
-        expired_time = expired.strftime("%m/%d/%Y, %H:%M:%S")
-        conn.execute(f"DELETE from Token WHERE token='{token_toint}'")
-        conn.execute(f"INSERT INTO Token values('{token_toint}','{expired_time}','{name}')")
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            token_toint = int.from_bytes(token, byteorder='big', signed=False)
+            expired_time = expired.strftime("%m/%d/%Y, %H:%M:%S")
+            cursor.execute(f"DELETE from Token WHERE token='{token_toint}'")
+            cursor.execute(f"INSERT INTO `Token` values('{token_toint}','{expired_time}','{name}')")
         conn.commit()
         conn.close()
 
     def get_token(self, token):
-        conn = sqlite3.connect(self.database)
-        token_toint = int.from_bytes(token, byteorder='big', signed=False)
-        datas = conn.execute(f"SELECT expired,name from Token WHERE token='{token_toint}'").fetchone()
-        conn.commit()
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            token_toint = int.from_bytes(token, byteorder='big', signed=False)
+            cursor.execute(f"SELECT expired,name from Token WHERE token='{token_toint}'")
+            datas = cursor.fetchone()
         conn.close()
 
         if datas is None:
@@ -112,33 +129,37 @@ class DbAdapter:
             return expired_time, name
 
     def add_election(self, election_name, end_date, groups, choices):
-        conn = sqlite3.connect(self.database)
-        end_date_tostr = end_date.strftime("%m/%d/%Y, %H:%M:%S")
-        groups_tostr = ','.join(groups)
-        voters = ''
-        conn.execute(f"DELETE from Election WHERE election_name='{election_name}'")
-        conn.execute(f"INSERT INTO Election values('{election_name}','{end_date_tostr}', \
-                                                    '{groups_tostr}','{voters}')")
-        conn.execute(f"DROP TABLE IF EXISTS {election_name}")                                            
-        conn.execute(f"CREATE TABLE {election_name}(name text NOT NULL, \
-                                                    votes integer NOT NULL)")
-        
-        choices_list = []
-        if type(choices) is str:
-            for chioce in choices.split(','):
-                choices_list.append(choice)
-        elif type(choices) is list:
-            choices_list = choices
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            end_date_tostr = end_date.strftime("%m/%d/%Y, %H:%M:%S")
+            groups_tostr = ','.join(groups)
+            voters = ''
+            cursor.execute(f"DELETE from Election WHERE election_name='{election_name}'")
+            cursor.execute(f"INSERT INTO `Election` values('{election_name}','{end_date_tostr}', \
+                                                        '{groups_tostr}','{voters}')")
+            cursor.execute(f"DROP TABLE IF EXISTS {election_name}")                                            
+            cursor.execute(f"CREATE TABLE `{election_name}`(`name` text NOT NULL, \
+                                                            `votes` integer NOT NULL)")
             
-        for choice in choices_list:
-            conn.execute(f"INSERT INTO {election_name} values('{choice}',0)")
+            choices_list = []
+            if type(choices) is str:
+                for choice in choices.split(','):
+                    choices_list.append(choice)
+            elif type(choices) is list:
+                choices_list = choices
+                
+            for choice in choices_list:
+                cursor.execute(f"INSERT INTO `{election_name}` values('{choice}',0)")
 
         conn.commit()
         conn.close()
 
     def get_all_elections(self):
-        conn = sqlite3.connect(self.database)
-        datas = conn.execute(f"SELECT election_name from Election").fetchall()
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT election_name from Election")
+            datas = cursor.fetchall()
+        conn.close()
 
         elections = []
         for data in datas:
@@ -146,50 +167,53 @@ class DbAdapter:
         return elections
 
     def get_election(self, election_name):
-        conn = sqlite3.connect(self.database)
-        datas = conn.execute(f"SELECT end_date,groups,voters from Election WHERE election_name='{election_name}'").fetchone()
-        end_date = datetime.strptime(datas[0], "%m/%d/%Y, %H:%M:%S")
-        groups = datas[1]
-        voters = datas[2]
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT end_date,`groups`,voters from Election WHERE election_name='{election_name}'")
+            datas = cursor.fetchone()
 
-        datas = conn.execute(f"SELECT name,votes from {election_name}").fetchall()
-        votes = {}
-        for data in datas:
-            votes[data[0]] = data[1]
+            end_date = datetime.strptime(datas[0], "%m/%d/%Y, %H:%M:%S")
+            groups = datas[1]
+            voters = datas[2]
+
+            cursor.execute(f"SELECT name,votes from {election_name}")
+            datas = cursor.fetchall()
+            votes = {}
+            for data in datas:
+                votes[data[0]] = data[1]
+        conn.close()
 
         table = {'end_date':end_date, 'groups':groups, 'votes':votes, 'voters':voters}
         return table
 
     def add_vote(self, election_name, choice, voter):
-        conn = sqlite3.connect(self.database)
-        voters = conn.execute(f"SELECT voters from Election WHERE election_name='{election_name}'").fetchone()
-        if voters is None:
-            print(f'There no election name {election_name}')
-            conn.commit()
-            conn.close()
-            return
-        votes = conn.execute(f"SELECT votes from {election_name} WHERE name='{choice}'").fetchone()
-        if votes is None:
-            print(f'There no candidate name {choice}')
-            conn.commit()
-            conn.close()
-            return
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT voters from Election WHERE election_name='{election_name}'")
+            voters = cursor.fetchone()
+            if voters is None:
+                print(f'There no election name {election_name}')
+                conn.commit()
+                conn.close()
+                return
+            cursor.execute(f"SELECT votes from {election_name} WHERE name='{choice}'")
+            votes = cursor.fetchone()
+            if votes is None:
+                print(f'There no candidate name {choice}')
+                conn.commit()
+                conn.close()
+                return
 
-        if voters[0] == '':
-            voters = voter
-        else:
-            voters = voters[0] + f',{voter}'
-        sql = ''' UPDATE Election
-              SET voters = ?
-              WHERE election_name = ?'''
-        cur = conn.cursor()
-        cur.execute(sql, (voters, election_name))
+            if voters[0] == '':
+                voters = voter
+            else:
+                voters = voters[0] + f',{voter}'
+            sql = f"UPDATE Election SET voters = '{voters}' WHERE election_name = '{election_name}'"
+            cursor.execute(sql)
 
-        votes = votes[0] + 1
-        sql = f''' UPDATE {election_name}
-              SET votes = ?
-              WHERE name = ?'''
-        cur.execute(sql, (votes, choice))
+            votes = votes[0] + 1
+            sql = f" UPDATE {election_name} SET votes = '{votes}' WHERE name = '{choice}'"
+            cursor.execute(sql)
 
         conn.commit()
         conn.close()
