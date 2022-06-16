c7dc36e1cb34ff4e1eb227e57e3bc48efda3e4a0
b4435242 <b4435242@gmail.com>
2022-06-16 20:02:02 +0800

    2pc

--- install_rqlite_linux.sh
+++ install_rqlite_linux.sh
@@ -1,2 +1,7 @@
 curl -L https://github.com/rqlite/rqlite/releases/download/v7.5.1/rqlite-v7.5.1-linux-amd64.tar.gz -o rqlite-v7.5.1-linux-amd64.tar.gz
 tar xvfz rqlite-v7.5.1-linux-amd64.tar.gz
+
+git clone https://github.com/rqlite/pyrqlite.git
+cd pyrqlite
+sudo python3 setup.py install
+cd ..
